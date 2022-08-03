59d244b8f9abe4ce158e6c82c477b8bbd86830ea
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2022-08-02 20:28:30 -0500

    get strapi data by id, still need to pass populate, to populate components

--- app/routes/posts.jsx
+++ app/routes/posts.jsx
@@ -1,50 +1,45 @@
-import { json } from "@remix-run/node";
 import { Outlet, useLoaderData, Link } from "@remix-run/react";
 
 import React from "react";
 
-export const loader = async () => {
-  return json({
-    posts: [
-      {
-        id: 1,
-        slug: "my-first-post-1",
-        title: "My First Post",
-      },
-      {
-        id: 2,
-        slug: "my-second-post-2",
-        title: "A Mixtape I Made Just For You",
-      },
-      {
-        id: 3,
-        slug: "my-third-post-3",
-        title: "My Third Post",
-      },
-    ],
+export async function loader() {
+  console.log(process.env.STRAPI_BASE_URL);
+  console.log(process.env.STRAPI_TOKEN_API);
+
+  const getPostsUrl = `${process.env.STRAPI_BASE_URL}/api/posts`;
+
+  const res = await fetch(getPostsUrl, {
+    headers: {
+      Authorization: `Bearer ${process.env.STRAPI_TOKEN_API}`,
+    },
   });
-};
+
+  const { data } = await res.json();
+  return { posts: data };
+}
 
 export default function () {
   const { posts } = useLoaderData();
-
-  console.log(posts, "from hook");
 
   return (
     <div>
       <h1>Posts Page</h1>
       <Link to="/">Back Home</Link>
-  
-        <h2>Posts</h2>
-        <aside>
-          <ul>
-            {posts.map((post) => (
+
+      <h2>Posts</h2>
+      <aside>
+        <ul>
+          {posts.map((post) => {
+            const { slug, title } = post.attributes;
+            console.log(slug, title);
+            return (
               <li key={post.id}>
-                <Link to={`/posts/${post.slug}`}>{post.title}</Link>
+                <Link to={`/posts/${post.id}`}>{title}</Link>
               </li>
-            ))}
-          </ul>
-        </aside>
+            );
+          })}
+        </ul>
+      </aside>
       <section>
         <Outlet />
       </section>
